80ca7beacd988c8fdfa128546486786c8fe43560
AlkiMeow <118789475+AlkiMeow@users.noreply.github.com>
2022-11-27 14:49:07 -0500

    Add files via upload

---
+++ train_and_test/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt-get install python3-pip
+sudo pip3 install virtualenv
+python3 -m venv cassper
+source cassper/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+
